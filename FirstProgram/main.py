5c203af34f52650aba982298df4f122904248f85
Bojan9597 <bojanpejic97@gmail.com>
2023-06-21 00:08:51 +0200

    Trying to correct mistakes

--- FirstProgram/main.py
+++ FirstProgram/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     login_window = LoginWindow()
 
     login_window.loginSuccessful.connect(main_window.handleLogin)
-
+    login_window.fileSelected.connect(main_window.handleFileSelected)
     main_window.show()
     login_window.show()
 
