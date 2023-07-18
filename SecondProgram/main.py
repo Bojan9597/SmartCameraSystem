6fbf0efebed731f1abcfddce5e0f98dfed5e184f
Bojan9597 <bojanpejic97@gmail.com>
2023-07-18 22:38:13 +0200

    some error removed

--- SecondProgram/main.py
+++ SecondProgram/main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide2.QtWidgets import QApplication
-from MainWindowPTZ import MainWindowPTZ
+from MainWindowPtz import MainWindowPTZ
 from MainWindowWA import MainWindowWA
 
 if __name__ == '__main__':
